8dd780c1028a97881893376343a4d919992d2588
robinplavila <robin.plavila@generalinternet.ca>
2025-10-29 17:45:23 -0700

    added Wingdings2

--- typography/tokens.umd.js
+++ typography/tokens.umd.js
@@ -8,6 +8,8 @@
     Vollkorn14:        { family: "Vollkorn",   style: "normal",    size: 14 },
     AptosSerifBold12:  { family: "AptosSerif", style: "bold",      size: 12 },
     AptosSerifReg14:   { family: "AptosSerif", style: "normal",    size: 14 },
-    AptosSerifReg12:   { family: "AptosSerif", style: "normal",    size: 12 }
+    AptosSerifReg12:   { family: "AptosSerif", style: "normal",    size: 12 },
+    // Wingdings 2 Boxed Checkmark (☑, U+2611)
+    Wingdings2BoxedCheck: { family: "Wingdings2", style: "normal", size: 16, char: "\u2611", html: "&#9745;" }
   };
 })(window);
